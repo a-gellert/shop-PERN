00ed07c96c9f60af64a331a5ad88076650126cf4
Gellert <sever6131@ya.ru>
2021-12-07 19:42:15 +0600

    controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,7 @@
+class UserController {
+  async signup(req, res) {}
+  async login(req, res) {}
+  async check(req, res) {}
+}
+
+module.exports = new UserController();
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const router = new Router();
+const userController = require("../controllers/userController");
 
-router.post("/signup");
-router.post("/login");
-router.get("/auth", (req, res) => res.json({ message: "All Working!" }));
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
+router.get("/auth", userController.check);
 
 module.exports = router;
